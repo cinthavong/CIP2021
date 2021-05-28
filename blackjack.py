5c34a1354e6a97fbde180377f8351f2b525eb97b
mike-costales <81458086+mike-costales@users.noreply.github.com>
2021-05-27 22:55:20 -0700

    Updated hit/stand code

--- blackjack.py
+++ blackjack.py
@@ -17,7 +17,7 @@ NUM_RANDOM = 3
 
 def main():
     # Let's get started with some liquid courage
-    print("I still remember how to do a print statement!\nWhew - I deserve a drink!")
+    print("I still remember how to do a print statement!\nWhew - I deserve a drink!\n")
 
     # TODO: Hey Matt - how do we play Blackjack again?
     # type of suit
@@ -31,27 +31,30 @@ def main():
 
     # Let's beat the dealer
     player_score = 0
-    print("player_score is type ", type(player_score))
+    # print("player_score is type ", type(player_score))
     dealer_score = 0
 
     # TODO: Let's bring the Las Vegas Welcome to Matt
     welcome_sign = SimpleImage('images/welcome_sign.png')
     welcome_sign.show()
 
-    print("Welcome to Vegas! Let's play Blackjack!")
+    print("Welcome to Vegas! Let's play Blackjack!\n")
 
     # dealing a card to the player
     print("Here are two cards.")
+
     # player_card_one = input(random.randint(MIN_RANDOM, MAX_RANDOM))
     player_card_one = random.randint(MIN_RANDOM, MAX_RANDOM)
     print("First card is ", str(player_card_one))
     # print("card one is type: ", type(player_card_one))
+
     # player_card_two = input(random.randint(MIN_RANDOM, MAX_RANDOM))
     player_card_two = random.randint(MIN_RANDOM, MAX_RANDOM)
     print("Second card is ", str(player_card_two))
     # print("card two is type: ", type(player_card_two))
+
     player_card_value = player_card_one + player_card_two
-    print("You have " + str(player_card_value))
+    print("You have " + str(player_card_value) + "\n")
     # print("player_card_value is type: ", type(player_card_value))
 
     # update the player score
@@ -67,12 +70,15 @@ def main():
 
     # if player score is under 21, ask them if they want to hit or stand
     while player_score < 21:
-        choice = str(input("Would you like to hit or stand? "))
-        if hit_choice() == 'hit':
+        hit_choice = str(input("Would you like to hit or stand? "))
+        if hit_choice == 'hit':
         # if choice == 'hit'
-            player_card = random.randint(MIN_RANDOM)
+            player_card = random.randint(MIN_RANDOM, MAX_RANDOM)
+            print("Next card is " + str(player_card))
             player_cards.append(player_card)
-        if stand_choice() == 'stand':
+            player_card_value += player_card
+            print("You have " + str(player_card_value))
+        if hit_choice == 'stand':
         # elif choice == 'stand'
             # show both cards
             print("DEALER CARDS: ")
